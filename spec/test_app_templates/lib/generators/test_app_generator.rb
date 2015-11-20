269879f8c5a98c6d315207610d0b03106492a59e
Ronan McHugh <mchugh.r@gmail.com>
2015-11-20 15:29:18 +0100

    In the test app we want to use a blank catalog controller

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,7 +1,7 @@
         require 'rails/generators'
 
         class TestAppGenerator < Rails::Generators::Base
-          source_root "./spec/test_app_templates"
+          source_root "../spec/test_app_templates"
 
           # if you need to generate any additional configuration
           # into the test app, this generator will be run immediately
@@ -10,9 +10,12 @@
           def install_blacklight
             say_status('warning', 'GENERATING BLACKLIGHT', :yellow)
             generate 'blacklight:install'
+            remove_file 'app/controllers/catalog_controller.rb'
+            copy_file 'catalog_controller.rb', 'app/controllers/catalog_controller.rb', '-f'
           end
 
           def install_dtu_common
+            generate 'dtu:install'
             generate 'dtu:solr_config_files', '-f'
           end
         end
