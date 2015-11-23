64009c1806b51dc56a5022d2619f987093ddb77d
Ronan McHugh <mchugh.r@gmail.com>
2015-11-23 14:56:59 +0100

    rspec setup

---
+++ spec/helpers/highlighting_spec.rb
@@ -0,0 +1,24 @@
+describe Highlighting do
+
+  before do
+    class HighlightingTestClass
+      include Highlighting
+
+    end
+  end
+
+  let(:solr_response) { { 'highlighting' => highlighting_values } }
+  let(:document) { HighlightingTestClass.new({ id: '2059', cluster_id_ss: '598732111' }, solr_response ) }
+  let(:highlighting_values) { {"2059"=>{"abstract_ts"=>["highlight value 1"]}} }
+
+  describe 'has_highlight_field?' do
+    it 'is able to find highlights corresponding to doc["id"]' do
+      expect(document.has_highlight_field?('abstract_ts')).to eq true
+    end
+  end
+  describe 'highlight_field' do
+    it 'is able to find highlights corresponding to doc["id"]' do
+      expect(document.highlight_field('abstract_ts')).to eq  ["highlight value 1"]
+    end
+  end
+end
