10753e1697ca338895005b3479b67d2fab3e0625
Matt Zumwalt <matt@databindery.com>
2015-11-24 12:34:42 +0100

    Dtu::CatalogBehavior does not declare any facet, index or show fields so host apps can control ordering

--- app/controllers/concerns/dtu/catalog_behavior.rb
+++ app/controllers/concerns/dtu/catalog_behavior.rb
@@ -1,10 +1,17 @@
 module Dtu
+  # Includes BlacklightCatalog and provides common Dtu configuration
+  # You must declare your own index, facet, and show fields in the host class (ie. CatalogController)
+  # because ordering is relevant when you call `add_index_field`, `add_facet_field` and `add_show_field`
+  # For examples of how to use those methods, see Blacklight's default CatalogController or see
+  # dtu_blacklight_common/spec/test_app_templates/catalog_controller.rb
   module CatalogBehavior
     extend ActiveSupport::Concern
     included do
       include Blacklight::Catalog
 
       configure_blacklight do |config|
+        # Ensure I18n load paths are loaded
+        Dir[Rails.root + 'config/locales/**/*.{rb,yml}'].each { |path| I18n.load_path << path }
 
         # items to show per page, each number in the array represent another option to choose from.
         config.per_page = [10,20,50]
@@ -26,30 +33,10 @@ module Dtu
         # solr field configuration for document/show views
         config.show.title_field = 'title_ts'
 
-
-        config.add_facet_field 'pub_date_tsort', :range => {
-            :num_segments => 3,
-            :assumed_boundaries => [1900, Time.now.year + 2]
-        }
-        config.add_facet_field 'author_facet', :limit => 10
-        config.add_facet_field 'journal_title_facet', :limit => 10
-
-
         # Have BL send all facet field names to Solr, which has been the default
         # previously. Simply remove these lines if you'd rather use Solr request
         # handler defaults, or have no facets.
         config.add_facet_fields_to_solr_request!
-
-        # INDEX FIELDS
-
-        config.add_index_field 'doi_ss'
-
-        # SHOW FIELDS
-
-        config.add_show_field 'subtitle_ts'
-        config.add_show_field 'doi_ss'
-        config.add_show_field 'abstract_ts'
-        config.add_show_field 'isbn_ss'
 
         # SORTING
 
--- spec/test_app_templates/catalog_controller.rb
+++ spec/test_app_templates/catalog_controller.rb
@@ -3,4 +3,29 @@ class CatalogController < ApplicationController
 
   include Dtu::CatalogBehavior
 
+  # Declare Facet, Index and Show fields for use in tests
+  # These can't be declared by Dtu::CatalogBehavior because ordering is relevant
+  # -- we want the host apps to be able to control the order of these fields.
+  configure_blacklight do |config|
+
+    ##
+    # FACET FIELDS
+    config.add_facet_field 'pub_date_tsort', :range => {
+                                               :num_segments => 3,
+                                               :assumed_boundaries => [1900, Time.now.year + 2]
+                                           }
+    config.add_facet_field 'author_facet', :limit => 10
+    config.add_facet_field 'journal_title_facet', :limit => 10
+
+    ##
+    # INDEX FIELDS
+    config.add_index_field 'doi_ss'
+
+    ##
+    # SHOW FIELDS
+    config.add_show_field 'subtitle_ts'
+    config.add_show_field 'doi_ss'
+    config.add_show_field 'abstract_ts'
+    config.add_show_field 'isbn_ss'
+  end
 end
