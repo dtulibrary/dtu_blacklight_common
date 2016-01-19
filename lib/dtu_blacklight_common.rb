28bb500adfa3d75855608e27d525dee066b648e8
Ronan McHugh <mchugh.r@gmail.com>
2016-01-19 15:46:40 +0100

    add font awesome to common gem

--- lib/dtu_blacklight_common.rb
+++ lib/dtu_blacklight_common.rb
@@ -1,6 +1,7 @@
 require "dtu_blacklight_common/engine"
 require 'blacklight'
 require 'twitter-typeahead-rails'
+require 'font-awesome-rails'
 
 module DtuBlacklightCommon
   extend ActiveSupport::Autoload
