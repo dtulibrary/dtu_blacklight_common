7e49b51668bdd9048b28e73a001014ebb277f66d
Ronan McHugh <mchugh.r@gmail.com>
2016-04-01 10:42:33 +0200

    add affiliation handling to document class

--- app/models/dtu/solr_document.rb
+++ app/models/dtu/solr_document.rb
@@ -3,5 +3,43 @@ module Dtu
     include Blacklight::Solr::Document
     include Dtu::SolrDocument::Highlighting
     include Dtu::SolrDocument::Identifiers
+
+    def authors_with_affiliations
+      contributors_with_affiliations(authors, 'author')
+    end
+
+    def editors_with_affiliations
+      contributors_with_affiliations(editors, 'editor')
+    end
+
+    # Given a group of contributors and a role
+    # Return a hash with the contributor and their affiliation index
+    # @param contributors Array
+    # @param role String author | editor
+    # @return Hash e.g. { 'author1' => 3 }
+    def contributors_with_affiliations(contributors, role)
+      output = {}
+      contributors.map.with_index do |cont, i|
+        output[cont] = affiliation_associations[role][i]
+      end
+      output
+    end
+
+    def authors
+      self['author_ts'] || []
+    end
+
+    def editors
+      self['editor_ts'] || []
+    end
+
+    def affiliations
+      self['affiliation_ts'] || []
+    end
+
+    # Convert the affiliations associations json to a Hash
+    def affiliation_associations
+      JSON.parse(self['affiliation_associations_json']) || {}
+    end
   end
 end
---
+++ spec/models/dtu/solr_document_spec.rb
@@ -0,0 +1,50 @@
+describe Dtu::SolrDocument do
+  describe 'affiliations' do
+    let(:source_doc) { {} }
+    let(:document) { Dtu::SolrDocument.new(source_doc) }
+    describe '#authors_with_affiliations' do
+      let(:source_doc) {{ 'author_ts' => [
+        "Matos, T.",
+        "Senkbeil, Silja",
+        "Mendonça, A.",
+        "Queiroz, J. A.",
+        "Kutter, Jörg Peter",
+        "Bulow, L."
+      ],
+      'affiliation_ts' => [
+        'Department of Micro- and Nanotechnology, Technical University of Denmark',
+        'ChemLabChip, Department of Micro- and Nanotechnology, Technical University of Denmark',
+        'Lund University',
+        'University of Beira Interior'
+      ],
+      'affiliation_associations_json' => '{"editor":[],"supervisor":[],"author":[2,0,3,3,0,2]}'
+      }}
+      subject { document.authors_with_affiliations }
+      it { should be_a Hash }
+      it 'preserves the correct ordering' do
+        expect(subject.keys.first).to eql 'Matos, T.'
+        expect(subject.keys.last).to eql 'Bulow, L.'
+      end
+      it 'adds footnotes for the relevant institution' do
+        expect(subject.values.first).to eql 2
+      end
+    end
+    describe 'editors_with_affiliations' do
+      let(:source_doc){{
+        'affiliation_associations_json' => '{"editor":[null,2],"supervisor":[],"author":[null,null]}',
+        'editor_ts' => ["Kimmel, A.", "Oliver, B."]
+        }}
+      subject { document.editors_with_affiliations }
+      it 'preserves the correct ordering' do
+        expect(subject.keys.first).to eql 'Kimmel, A.'
+        expect(subject.keys.last).to eql 'Oliver, B.'
+      end
+      it 'returns nil for null values' do
+        expect(subject.values.first).to eql nil
+      end
+      it 'returns numbers when relevant' do
+        expect(subject.values.last).to eql 2
+      end
+    end
+  end
+end
